6d5cbcf5ec31ef171e06dd0a765394eb740fd01f
Mao Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2012-02-02 22:31:30 +0900

    added: rspec test for NdcTree::Node

--- spec/ndc_tree_spec.rb
+++ spec/ndc_tree_spec.rb
@@ -1,7 +1,36 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "NdcTree" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "NdcTree::Node" do
+
+  before do
+    @tree = NdcTree::Node.new
   end
+
+  it "should initialize" do
+    @tree.should be_instance_of NdcTree::Node
+  end
+
+  it "should has name" do
+    @tree.name.should be_instance_of String
+  end
+
+  it "should not has accessor of name" do
+    (defined? @tree.name="newname").should be_nil
+  end
+
+  it "should has weight" do
+    @tree.weight.should > 0
+  end
+
+  it "should has content" do
+    @tree.content.should be_instance_of Hash
+  end
+
+  it "should has accessor of weight" do
+    @tree.weight = 1
+    @tree.weight.should == 1
+    @tree.weight+=1
+    @tree.weight.should == 2
+  end
+  
 end
