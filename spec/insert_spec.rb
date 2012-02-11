382313ee220b5ca3efcfe8501a297f5e0094f4be
Mao Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2012-02-12 05:30:32 +0900

    changed: spec/insert_spec.rb

--- spec/insert_spec.rb
+++ spec/insert_spec.rb
@@ -1,13 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "NdcTree::Node" do
+describe "NdcTree <<" do
 
   subject { @tree = NdcTree << [ "913.6" ] }
 
   it { should be_instance_of NdcTree::Node }
+
+  its(:name) { should == "root" }
   its(:nodes) {
     should be_instance_of Array
     should have(5).items
+  }
+  its(:children) {
+    should have(1).items
+    subject.first.name.should  == "9**"
   }
 
 end
